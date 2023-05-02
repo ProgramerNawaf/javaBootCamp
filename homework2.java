ea630d644434e8c4b435f7aaf5e483bbf204cbbe
ProgramerNawaf <105772524+ProgramerNawaf@users.noreply.github.com>
2023-05-02 06:23:59 +0300

    Add files via upload
    Homework2

---
+++ homework2.java
@@ -0,0 +1,123 @@
+import java.util.Scanner;
+
+public class homework2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		//exer1
+//		System.out.print("Input first number:");
+//		int num1 = s.nextInt();
+//		System.out.print("Input second number:");
+//		int num2 = s.nextInt();
+//		
+//		System.out.println(num1+" + "+num2 +" = "+ (num1+num2));
+//		System.out.println(num1+" - "+num2 +" = "+ (num1-num2));
+//		System.out.println(num1+" * "+num2 +" = "+ (num1*num2));
+//		System.out.println(num1+" / "+num2 +" = "+ (num1/num2));
+//		
+		//exer2
+//		System.out.println("Input a String:");
+//		String word = s.nextLine();
+//		System.out.println(word.toLowerCase());
+		
+		//exer3
+//		System.out.println("Input a String:");
+//		String wordd = s.nextLine();
+//		System.out.print("Input number:");
+//		int num = s.nextInt();
+//		System.out.println(wordd.charAt(num));
+		
+		//exer4
+//		System.out.print("Input number:");
+//		int numm = s.nextInt();
+//		if(numm%2 == 1)
+//			System.out.println("0");
+//		else
+//			System.out.println("1");
+		
+		//exer5
+//		System.out.print("Input your role:");
+//		String role = s.nextLine();
+//		
+//		if(role.equalsIgnoreCase("admin")) {
+//			System.out.print("welcome admin");
+//		}else if(role.equalsIgnoreCase("superuser")) {
+//			System.out.print("welcome superuser");
+//		}else if(role.equalsIgnoreCase("user")) {
+//			System.out.print("welcome user");
+//		}else {
+//			System.out.print("not a valid role");
+//		}
+		
+		//exer6
+//		System.out.print("Input first number:");
+//		int num11 = s.nextInt();
+//		System.out.print("Input second number:");
+//		int num22 = s.nextInt();
+//		System.out.print("Input thrid number:");
+//		int num33 = s.nextInt();
+//		
+//		if((num11+num22)==num33)
+//			System.out.println(true);
+//		else
+//			System.out.println(false);
+		
+		//exer7
+//		System.out.print("Input first number:");
+//		int num111 = s.nextInt();
+//		System.out.print("Input second number:");
+//		int num222 = s.nextInt();
+//		System.out.print("Input thrid number:");
+//		int num333 = s.nextInt();
+//		
+//		if(num111 > num222) {
+//			if(num111>num333) {
+//				System.out.print(num111);
+//			}else {
+//				System.out.print(num333);
+//			}
+//		}else {
+//			if(num222>num333) {
+//				System.out.print(num222);
+//			}else {
+//				System.out.print(num333);
+//			}
+//		}
+		
+		//exer8
+//		System.out.print("Input number:");
+//		int day = s.nextInt();
+//		switch(day)
+//        {
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            case 3:
+//                System.out.println("Wednesday");
+//                break;
+//            case 4:
+//                System.out.println("Thursday");
+//                break;
+//            case 5:
+//                System.out.println("Friday");
+//                break;
+//            case 6:
+//                System.out.println("Saturday");
+//                break;
+//            case 7:
+//                System.out.println("Sunday");
+//                break;
+//            default:
+//                System.out.println("invalid input");
+//                break;
+//        }
+		
+		
+		
+	}
+
+}
