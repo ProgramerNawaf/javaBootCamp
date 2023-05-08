c2462023c5f85329247463b4ae5efd5b9605174f
ProgramerNawaf <105772524+ProgramerNawaf@users.noreply.github.com>
2023-05-08 14:28:33 +0300

    Update Account.java

--- Homework7/Account.java
+++ Homework7/Account.java
@@ -1,38 +1,82 @@
-import java.sql.SQLOutput;
+public class Account {
+    private String id;
+    private String name;
+    private int balance;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
+    public Account(){
 
-          //Employee testing
-//        Employee e1 = new Employee("22","Nawaf",50);
-//        System.out.println(e1.getAnnualSalary());
-//        System.out.println(e1.getSalary());
-//        e1.raisedSalary(50);
-//        System.out.println(e1.getSalary());
+    }
 
-        //Account testing
-//
-//            Account a1 = new Account("11", "Nawaf", 50);
-//            Account a2 = new Account("22", "faisal");
-//
-//            System.out.println(a1.getBalance());
-//            System.out.println(a2.getBalance());
-//            System.out.println("------------------");
-//            a1.credit(25);
-//            a2.debit(10);
-//            a1.getBalance();
-//            a2.getBalance();
-//            System.out.println("------------------");
-//            a1.TransferTo(a2, 25);
-//            a2.getBalance();
-//            //cant credit or transfer because he has no balance
-//            a1.credit(10);
-//            a1.TransferTo(a2,10);
-//
-//            System.out.println("------------------");
-//            System.out.println(a1.toString() + " "+ a2.toString());
+    public Account(String id, String name) {
+        this.id = id;
+        this.name = name;
+        this.balance = 0;
+    }
+    public Account(String id, String name, int balance) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
 
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBalance() {
+        System.out.println(this.toString());
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int credit (int amount ) {
+        try{
+        if(this.balance == 0 ) {
+            throw new Exception("Can not credit !Account has zero Balance");
+        }
+            this.balance -= amount;
+            return balance;
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return 0;
+        }
+
+    }
+    public int debit (int amount ){
+        this.balance +=amount;
+        return balance;
+    }
+
+    public int TransferTo(Account another , int amount ){
+
+        another.debit(amount);
+        this.credit(amount);
+        System.out.println(this.toString());
+        return this.balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account: " +
+
+                "name=" + name +
+                ", balance=" + balance ;
     }
 }
