90e8a6c5d2639dee3dbf14d51da1b070e07df2f1
ProgramerNawaf <105772524+ProgramerNawaf@users.noreply.github.com>
2023-05-08 13:40:52 +0300

    Create main.java

---
+++ Homework7/main.java
@@ -0,0 +1,30 @@
+import java.sql.SQLOutput;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+          //Employee testing
+//        Employee e1 = new Employee("22","Nawaf",50);
+//        System.out.println(e1.getAnnualSalary());
+//        System.out.println(e1.getSalary());
+//        e1.raisedSalary(50);
+//        System.out.println(e1.getSalary());
+
+        //Account testing
+//        Account a1 = new Account("11","Nawaf",50);
+//        Account a2 = new Account("22","faisal");
+//
+//        System.out.println("Nawaf balance: "+a1.getBalance());
+//        System.out.println("Faisal balance: "+a2.getBalance());
+//        System.out.println("------------------");
+//        a1.credit(25);
+//        a2.debit(10);
+//        System.out.println("Nawaf balance: "+a1.getBalance());
+//        System.out.println("Faisal balance: "+a2.getBalance());
+//        System.out.println("------------------");
+//        System.out.println("Nawaf balance "+a1.TransferTo(a2,25));
+//        System.out.println("Faisal balance: "+a2.getBalance());
+    }
+}
