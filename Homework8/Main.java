1d6e8c563a6c21eb76e2e3adba8820e9b1726f8a
ProgramerNawaf <105772524+ProgramerNawaf@users.noreply.github.com>
2023-05-09 14:00:57 +0300

    Create Main.java

---
+++ Homework8/Main.java
@@ -0,0 +1,32 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+            Square s1 = new Square("Orange",false,3);
+//        square testing
+//        System.out.println(s1.getArea());
+//        System.out.println(s1.getSide());
+//        System.out.println(s1.getPerimeter());
+//        System.out.println(s1.toString());
+//        Square s2 = new Square(4);
+//        System.out.println("-------------------");
+//        System.out.println(s2.getArea());
+//        System.out.println(s2.getSide());
+//        System.out.println(s2.getPerimeter());
+//        System.out.println(s2.toString());
+
+        //square
+//        Circle c1 = new Circle(5);
+//        System.out.println(c1.toString());
+//        System.out.println(c1.getArea());
+//        System.out.println(c1.getPerimeter());
+
+//        Rectangle r1 = new Rectangle(4,4);
+//        System.out.println(r1.toString());
+//        System.out.println(r1.getArea());
+//        System.out.println(r1.getPerimeter());
+
+    }
+}
