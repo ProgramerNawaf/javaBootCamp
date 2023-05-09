39b9309195314e4b8609d9ba0e0bc589a091fc1b
ProgramerNawaf <105772524+ProgramerNawaf@users.noreply.github.com>
2023-05-09 14:04:29 +0300

    added test cases

--- Homework8/Main.java
+++ Homework8/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
 
             Square s1 = new Square("Orange",false,3);
-//        square testing
+//        Square testing
 //        System.out.println(s1.getArea());
 //        System.out.println(s1.getSide());
 //        System.out.println(s1.getPerimeter());
@@ -17,12 +17,12 @@ public class Main {
 //        System.out.println(s2.getPerimeter());
 //        System.out.println(s2.toString());
 
-        //square
+        //Circle testing
 //        Circle c1 = new Circle(5);
 //        System.out.println(c1.toString());
 //        System.out.println(c1.getArea());
 //        System.out.println(c1.getPerimeter());
-
+           //Rectangle testing
 //        Rectangle r1 = new Rectangle(4,4);
 //        System.out.println(r1.toString());
 //        System.out.println(r1.getArea());
